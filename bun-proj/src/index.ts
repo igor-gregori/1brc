27bd45e087243d4752122150376040a457dd404d
Igor Gregori <igsilvacarlos@gmail.com>
2026-03-07 00:38:03 -0300

    write my first implementation

---
+++ bun-proj/src/index.ts
@@ -0,0 +1,46 @@
+const file = Bun.file("../measurements/measurements.txt");
+
+console.time("File I/O API method");
+
+const content = await file.text();
+
+type Results = {
+  [key: string]: {
+    min: number;
+    max: number;
+    sum: number;
+    totalSamples: number;
+  };
+};
+
+let results: Results = {};
+
+for (const row of content.split("\n")) {
+  const [station, strMeasurement] = row.split(";");
+
+  if (!station || !strMeasurement) {
+    throw new Error("Invalid data");
+  }
+
+  const measurement = Number(strMeasurement);
+
+  if (results[station] === undefined) {
+    results[station] = {
+      min: measurement,
+      max: measurement,
+      sum: measurement,
+      totalSamples: 1,
+    };
+  } else {
+    if (results[station].min > measurement) {
+      results[station].min = measurement;
+    }
+    if (results[station].max < measurement) {
+      results[station].max = measurement;
+    }
+    results[station].sum += measurement;
+    results[station].totalSamples++;
+  }
+}
+
+console.timeEnd("File I/O API method");
