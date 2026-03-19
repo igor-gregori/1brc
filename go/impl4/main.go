fee14f6ebc597cba431e66839487aecca0dd7e4e
Igor Gregori <igsilvacarlos@gmail.com>
2026-03-19 20:19:03 -0300

    write the base of impl4

---
+++ go/impl4/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Result struct {
+	min          float64
+	max          float64
+	sum          float64
+	totalSamples int64
+}
+
+var gm []map[string]Result
+
+func main() {
+	file, err := os.Open("../measurements/measurements-100M.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	start := time.Now()
+
+	buf := make([]byte, 1024*1024) // 1MB buffer
+	leftover := make([]byte, 0)
+
+	chunckCounter := 0
+
+	var wg sync.WaitGroup
+
+	for {
+		n, _ := file.Read(buf)
+		if n == 0 {
+			break
+		}
+
+		data := append(leftover, buf[:n]...)
+
+		lastNewline := bytes.LastIndexByte(data, '\n')
+		if lastNewline == -1 {
+			leftover = data
+			continue
+		}
+
+		chunk := string(data[:lastNewline])
+		wg.Add(1)
+		go processChunk(chunk, &wg)
+
+		chunckCounter++
+
+		leftover = data[lastNewline+1:]
+	}
+	wg.Wait()
+
+	fmt.Println("Chunk counter:", chunckCounter)
+	fmt.Println("Length of Global Map", len(gm))
+
+	end := time.Now()
+	timeSpent := end.Sub(start)
+	fmt.Println("Time spent", timeSpent.Milliseconds(), "ms")
+}
+
+func processChunk(chunk string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	m := make(map[string]Result)
+	lines := strings.Split(chunk, "\n")
+	for _, line := range lines {
+		parts := strings.Split(line, ";")
+		station, strMeasurement := parts[0], parts[1]
+
+		measurement, err := strconv.ParseFloat(strMeasurement, 64)
+		if err != nil {
+			fmt.Println("Error: The string is not a valid number!")
+			break
+		}
+
+		result, ok := m[station]
+		if ok {
+			m[station] = Result{
+				min:          math.Min(result.min, measurement),
+				max:          math.Max(result.max, measurement),
+				sum:          result.sum + measurement,
+				totalSamples: result.totalSamples + 1,
+			}
+		} else {
+			m[station] = Result{
+				min:          measurement,
+				max:          measurement,
+				sum:          measurement,
+				totalSamples: 1,
+			}
+		}
+	}
+	gm = append(gm, m)
+}
